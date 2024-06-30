5bb400e93ec28726002034389d85909095cf9c44
Muhammad Idris <idris@evercore.technology>
2024-06-30 15:06:07 +0700

    partial prerendering

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,10 @@
-export default function Page() {
+import { fetchCardData, fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function Page() {
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
+  // const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+
   return (
     <p>Invoices Page</p>
   );
